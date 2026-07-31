679e948a07cd8f723e104c238f61c49251f4f44d
Abdul Hasib Zahid <abdulhasibzahid@gmail.com>
2026-07-31 18:22:03 +0600

    Served Updated

--- server/models/Settings.js
+++ server/models/Settings.js
@@ -22,53 +22,65 @@ const settingsSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+const DEFAULT_SETTINGS = {
+  classes: [
+    { name: "Play Group", code: "P", order: 1 },
+    { name: "Nursery", code: "N", order: 2 },
+    { name: "KG", code: "K", order: 3 },
+    { name: "STD-I", code: "I", order: 4 },
+    { name: "STD-II", code: "J", order: 5 },
+    { name: "STD-III", code: "L", order: 6 },
+    { name: "STD-IV", code: "M", order: 7 },
+    { name: "STD-V", code: "V", order: 8 },
+  ],
+  sections: ["A", "B", "C"],
+  subjects: [
+    "Arabic",
+    "Math",
+    "English",
+    "Bangla",
+    "BGS",
+    "Science",
+    "MDP",
+    "Islamic Studies",
+  ],
+  examNames: [
+    "Half Yearly",
+    "Year Final",
+    "Model Test",
+    "Monthly Assessment",
+    "Admission Test",
+  ],
+  paymentMethods: [
+    "Cash",
+    "bKash",
+    "Nagad",
+    "Rocket",
+    "Bank",
+    "Cheque",
+    "Card",
+    "Online",
+    "Other",
+  ],
+  academicSessions: ["2025", "2026", "2027"],
+  currentSession: "2026",
+};
+
 settingsSchema.statics.getSettings = async function () {
   let settings = await this.findOne();
   if (!settings) {
-    settings = await this.create({
-      classes: [
-        { name: "Play Group", code: "P", order: 1 },
-        { name: "Nursery", code: "N", order: 2 },
-        { name: "KG", code: "K", order: 3 },
-        { name: "STD-I", code: "I", order: 4 },
-        { name: "STD-II", code: "J", order: 5 },
-        { name: "STD-III", code: "L", order: 6 },
-        { name: "STD-IV", code: "M", order: 7 },
-        { name: "STD-V", code: "V", order: 8 },
-      ],
-      sections: ["A", "B", "C"],
-      subjects: [
-        "Arabic",
-        "Math",
-        "English",
-        "Bangla",
-        "BGS",
-        "Science",
-        "MDP",
-        "Islamic Studies",
-      ],
-      examNames: [
-        "Half Yearly",
-        "Year Final",
-        "Model Test",
-        "Monthly Assessment",
-        "Admission Test",
-      ],
-      paymentMethods: [
-        "Cash",
-        "bKash",
-        "Nagad",
-        "Rocket",
-        "Bank",
-        "Cheque",
-        "Card",
-        "Online",
-        "Other",
-      ],
-      academicSessions: ["2025", "2026", "2027"],
-      currentSession: "2026",
-    });
+    return this.create(DEFAULT_SETTINGS);
   }
+
+  let changed = false;
+  for (const key of Object.keys(DEFAULT_SETTINGS)) {
+    if (settings[key] === undefined || settings[key] === null) {
+      settings[key] = DEFAULT_SETTINGS[key];
+      changed = true;
+    }
+  }
+  if (changed) await settings.save();
+
   return settings;
 };
 
